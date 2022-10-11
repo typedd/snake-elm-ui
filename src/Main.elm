8f77be84d9e00ea58572fcc5b38030dafb1238b8
AVZapevalov <avzapevalov@gmail.com>
2022-10-11 18:15:31 +0100

    asdf

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,6 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html)
 import Element exposing (..)
 import Element.Border as Border
 import Element.Background as Background
@@ -18,21 +17,18 @@ type alias Model =
     { x : Int
     , y : Int
     }
-
 init : () -> (Model, Cmd msg)
 init _ =
-    ({ x = 3, y = 3 }, Cmd.none)
+    ({ x = 20, y = 20 }, Cmd.none)
 
 update : msg -> Model -> (Model, Cmd msg)
 update msg model =
     (model, Cmd.none)
-
-view : Model -> Html msg
 view model =
     layout
         [ width fill
         , height fill
-        , Element.explain Debug.todo
+        --, Element.explain Debug.todo
         ]
     <|
         el
@@ -41,28 +37,39 @@ view model =
             column
                 []
             <|
-                [ row
-                    []
-                <|
-                    [ el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    , el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    , el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    ]
-                , row
-                    []
-                <|
-                    [ el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    , el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    , el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    ]
-                , row
-                    []
-                <|
-                    [ el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    , el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    , el [ Border.color <| rgb255 255 255 255, Border.width 2, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
-                    ]
-            ]
+                List.indexedMap (\i row -> foo i ) ((List.repeat model.x cell))
+
+
+
+cell : Element msg
+cell = el [ Border.color <| rgb255 255 255 255, Border.width 1, Background.color <| rgb255 50 20 20, width (px 20), height (px 20) ] Element.none
+
+-- foo : Int -> Int -> Element msg
+-- foo xx yy = el [ Border.color <| rgb255 255 255 255, Border.width 1, (if (xx == 5) && (yy == 5) then Background.color <| rgb255 150 20 20 else Background.color <| rgb255 50 20 20), width (px 20), height (px 20) ] Element.none
+
+foo : Int -> Element msg
+foo xx = el [ Border.color <| rgb255 255 255 255, Border.width 1, (if (xx == 5) then Background.color <| rgb255 150 20 20 else Background.color <| rgb255 50 20 20), width (px 20), height (px 20) ] Element.none
+
+
+fieldRow : Int -> Element msg
+fieldRow n = Element.row [] (List.repeat n cell)
 
 subscriptions : a -> Sub msg
 subscriptions _ = Sub.none
+
+
+
+
+
+
+                --(List.repeat model.y (fieldRow model.x))
+               --List.map (\i row -> List.indexedMap i List.repeat model.x foo(i)))
+
+               --         List.map (\i -> foo(i)) (List.repeat model.x cell)
+
+                --foo (i))) 
+                 --   (List.repeat model.y (fieldRow model.x))
+                
+                ---(List.repeat model.x cell)
+
+                --List.Index.Map (\i row -> List.Index.Map (\j cell -> foo i j row) (List.repeat model.y (fieldRow model.x)))
